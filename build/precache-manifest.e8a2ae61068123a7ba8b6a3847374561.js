c20904f63c07eb2cd6ae6dfb22644c568d5f9949
Melanie Hendricks <melanie.r.hendricks@gmail.com>
2023-03-12 17:52:51 -0700

    added Q&A project

---
+++ build/precache-manifest.e8a2ae61068123a7ba8b6a3847374561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "615574cfedc179df75d714530a47829f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5f24f395b72468abf52",
+    "url": "/static/css/main.3e3be724.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f24f395b72468abf52",
+    "url": "/static/js/main.6d865436.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
---
+++ src/data.js
@@ -0,0 +1,32 @@
+const questions = [
+  {
+    id: 1,
+    title: "What is the oddest way you ever accidentally hurt yourself?",
+    info:
+      "I was studying for a bio test in bed one evening and lit a candle on my nightstand to set the mood. " +
+      " I passed out and shifted around in my sleep, " +
+      "which pushed one of my pillows on top of the candle but not far enough to cover it completely and extinguish the flame. " +
+      "I woke up to my bed on fire and the smell of my burning hair.",
+  },
+  {
+    id: 2,
+    title: "If you could make up a new holiday, what would it be?",
+    info: "This isn't exactly making up a holiday, but I would make Election Day a federal holiday.",
+  },
+  {
+    id: 3,
+    title: "If you could tell your past self one thing, what would it be?",
+    info: "You will be in college for a long time, longer than almost all of your friends, and that's okay.",
+  },
+  {
+    id: 4,
+    title: "What is one item on your bucket list?",
+    info: "BASE jumping in a wingsuit through the Alps with my best friend.",
+  },
+  {
+    id: 5,
+    title: "How do you make tough decisions?",
+    info: "I ask myself if/how this will affect me in 5 years time.",
+  },
+];
+export default questions;
